248908d084a5df31f14ae427e8826f9ec9af7e42
GOD akila <akiladananjaya79@gmail.com>
2020-03-22 08:09:30 +0530

    basic events

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { 
-  FaChessKnight,
-  FaCircle,
-  FaRegCircle,
-} from 'react-icons/fa';
-
 import Square from './components/Square';
 
 const generateGrid = () => {
-  return (
-    <Square gridIndex="22"/>
-  );
+  const SIZE = 4;
+  let gridMap = [];
+  for (let i=0; i<SIZE; i++){
+    let rowMap = [<tr></tr>];
+    for (let j=0; j<SIZE; j++){
+      rowMap.push(<Square key={`${i},${j}`} gridIndex={`${i},${j}`}/>);
+    }
+    gridMap = [gridMap, ...rowMap];
+  }
+  return gridMap;
 };
-
 
 function App() {
   return (
@@ -22,34 +21,10 @@ function App() {
     <table className="table-matrix" border="1" cellSpacing="0">
       <thead></thead>
       <tbody>
-        <tr>
-          {generateGrid()}
-        </tr>
+        {generateGrid()}
       </tbody>
     </table>
     </div>
-
-
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-
-
-
   );
 }
 
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import '../App.css';
+import { 
+  FaChessKnight,
+  FaCircle,
+  FaRegCircle,
+  FaChessKing,
+} from 'react-icons/fa';
 
 const Square = ({gridIndex}) => {
+  const [isPlayer, setIsPlayer] = useState(false);
+   
   return (
-    <td className="td"></td>
+    <td onClick={()=>setIsPlayer(!isPlayer)} className="td" id={gridIndex}>
+      {isPlayer ? <FaChessKing size="10"/> : null}
+    </td>
   );
 }
 
