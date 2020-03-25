4c510fef64671f589c1c3aedd2e07b0708dda75c
Thanuja Madushan <thanujafernando36@gmail.com>
2020-03-25 10:07:51 +0530

    Added knight path

--- src/controllers/knightController.js
+++ src/controllers/knightController.js
@@ -41,7 +41,7 @@ const getPossibleKnight = (KnightArray) => {
 
   var PossibleKnight = new Array(ArraySize);
   var PossibleKnightIndex = 0;
-  
+
   for (var a = 0; a < HorizontalVertical.length; a++) {
     if (KnightArray[0] + 2 == HorizontalVertical[a][0] || KnightArray[0] - 2 == HorizontalVertical[a][0]) {
       if (KnightArray[1] + 1 == HorizontalVertical[a][1] || KnightArray[1] - 1 == HorizontalVertical[a][1]) {
@@ -60,8 +60,42 @@ const getPossibleKnight = (KnightArray) => {
   return PossibleKnight;
 };
 
+//Knight path
+const getKnigthPath = (KnightOld, KnightNew) => {
+
+  var TKnight = KnightOld;
+  var HorizontalSign = 1;
+  var VerticalSign = 1;
+  var PathArray = new Array(3);
+  var PathArrayIndex = 0;
+  var HDifference = KnightNew[1] - KnightOld[1];
+  var VDifference = KnightNew[0] - KnightOld[0];
+  if (HDifference < 0) {
+    HorizontalSign = HorizontalSign * (-1);
+    HDifference = HDifference * (-1);
+  }
+  if (VDifference < 0) {
+    VerticalSign = VerticalSign * (-1);
+    VDifference = VDifference * (-1);
+  }
+  for (var i = 0; i < HDifference; i++) {
+
+    PathArray[PathArrayIndex] = [TKnight[0], TKnight[1] + (HorizontalSign)];
+    TKnight = PathArray[PathArrayIndex];
+    PathArrayIndex = PathArrayIndex + 1;
+  }
+  for (var j = 0; j < VDifference; j++) {
+
+    PathArray[PathArrayIndex] = [TKnight[0] + (VerticalSign), TKnight[1]];
+    TKnight = PathArray[PathArrayIndex];
+    PathArrayIndex = PathArrayIndex + 1;
+  }
+  return PathArray;
+}
+
 module.exports = {
   setup,
   getPossibleKnight,
   isLost,
+  getKnigthPath,
 };
